46aab51a4f76a67c3f2220ada0ed757da16da155
RAMYA357 <46557588+RAMYA357@users.noreply.github.com>
2019-01-11 09:46:27 +0530

    Create alph.py

---
+++ alph.py
@@ -0,0 +1,9 @@
+#alphabet
+ch=input()
+if ch.isalpha():
+	if ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
