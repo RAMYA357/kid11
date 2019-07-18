87fe99adb5041502f39ef77eddcf29ff41d0c938
RAMYA357 <46557588+RAMYA357@users.noreply.github.com>
2019-07-18 20:56:25 +0530

    Create b71.cpp

---
+++ b71.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	string s;
+	cin>>s;
+	int i,k;
+	cin>>k;
+	for(i=0;i<k;i++)
+	{
+		cout<<s[i];
+	}
+	return 0;
+}
