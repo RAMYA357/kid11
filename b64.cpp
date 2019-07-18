7d871d31a3834d766e466207b094b56991fb0dd5
RAMYA357 <46557588+RAMYA357@users.noreply.github.com>
2019-07-18 20:02:21 +0530

    Create b64.cpp

---
+++ b64.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<n;
+	}
+	else
+	{
+		cout<<n-1;
+	}
+	
+	return 0;
+}
