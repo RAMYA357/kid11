64dfd1ac0bb1cbd9b145b82c2af076dd4fb8103e
RAMYA357 <46557588+RAMYA357@users.noreply.github.com>
2019-07-23 17:58:01 +0530

    Create bi68.cpp

---
+++ bi68.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n%7==0)
+	{
+		cout<<"yes";
+	}
+	else{
+		cout<<"no";
+	}
+	return 0;
+}
