efc0408c080a59e48ea7b1998d2af1c792502a29
RAMYA357 <46557588+RAMYA357@users.noreply.github.com>
2019-07-23 18:01:36 +0530

    Create bi69.cpp

---
+++ bi69.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int m,n;
+	cin>>m>>n;
+	if((m-n)%2==0)
+	{
+		cout<<"even";
+	}
+	else
+	{
+		cout<<"odd";
+	}
+	return 0;
+}
